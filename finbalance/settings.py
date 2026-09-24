7afeae599cda057b5d7dc0f71f27a47c1feb3ad8
MopuK <stanislavgusev.sv@gmail.com>
2026-09-25 00:34:36 +0300

    fix: add render to allowed hosts

--- finbalance/settings.py
+++ finbalance/settings.py
@@ -12,10 +12,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "finbalance-fdvt.onrender.com"]
 
 CSRF_TRUSTED_ORIGINS = []
 
